60da3f68e2c2cbab98fb975cc8bdc1d78125195f
mikhsand <43511563+mikhsand@users.noreply.github.com>
2018-09-28 10:10:57 +0700

    proses
    tampilan pilihan makanan dan hobi

--- proses.php
+++ proses.php
@@ -1,4 +1,52 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>proses</title>
+</head>
+<body>
+<h2>Pilih Makanan</h2>
+		<form action="<?php echo $_SERVER['PHP_SELF'];?>"  method="POST">
+		<table>
+			<tr>
+				<td><input type="checkbox" name="makanan[]" value="baso">Baso</td>
+			</tr>
 
+			<tr>
+				<td><input type="checkbox" name="buah[]" value="jeruk">Jeruk</td>
+			</tr>
+			
+			<tr>
+				<td><input type="checkbox" name="laptop[]" value="acer">Acer</td>
+			</tr>
 
-?>
+			<tr>
+				<td><input type="submit" name="send" value="submit"></td>
+			</tr>
+			</table>
+			</form>
+
+	<h2>Pilih Hobi</h2>
+		<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+			<table>
+				<tr>
+				<td><input type="checkbox" name="hobi[]" value="renang">Renang</td>
+			</tr>
+
+			<tr>
+				<td><input type="checkbox" name="hobi[]" value="nari">Nari</td>
+			</tr>
+			
+			<tr>
+				<td><input type="checkbox" name="hobi[]" value="bola">Main Bola</td>
+			</tr>
+
+			<tr>
+				<td><input type="submit" name="send" value="submit"></td>
+			</tr>
+			</table>
+		</form>
+
+			
+</body>
+</html>
+
